7367bafc2a9cfb6f27f4c17629b3755b5262ad3e
Atif Asim <atifasim@Atifs-MacBook-Air.local>
2022-08-17 10:42:21 +0500

    new

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,7 +2,8 @@ let FaqHead = document.querySelectorAll(".faq_wrapper .faq .head");
 let myTimeline = new TimelineLite();
 let myTimeline2 = new TimelineLite();
 let myTimeline3 = new TimelineLite();
-
+let myTimeline4 = new TimelineLite();
+var controller = new ScrollMagic.Controller();
 myTimeline.staggerTo(".stagger1", 0.5, { opacity: 1, y: 0 }, 0.06);
 myTimeline.staggerTo(".stagger", 0.2, { opacity: 1, y: 0 }, 0.03);
 myTimeline2.to(".stagger2", 2, { opacity: 1 });
@@ -14,6 +15,40 @@ FaqHead.forEach((EachHead) => {
     Parent.classList.toggle("active");
   });
 });
+
+let scene = new ScrollMagic.Scene({
+  triggerElement: ".eco_system .grid_wrapper",
+  triggerHook: 0.5,
+  duration: 1000,
+})
+  .on("enter", function () {
+    document.querySelector(".eco_system .grid_wrapper").classList.add("active");
+  })
+
+  .addTo(controller);
+
+let scene2 = new ScrollMagic.Scene({
+  triggerElement: ".new",
+  triggerHook: 0.5,
+  duration: 1000,
+})
+  .on("enter", function () {
+    document.querySelector(".protcol_cards_wrapper").classList.add("active");
+  })
+
+  .addTo(controller);
+let scene3 = new ScrollMagic.Scene({
+  triggerElement: ".protocolagain",
+  triggerHook: 0.5,
+  duration: 1000,
+})
+  .on("enter", function () {
+    document
+      .querySelector(".protocolagain .protcol_cards_wrapper")
+      .classList.add("active");
+  })
+
+  .addTo(controller);
 
 window.addEventListener("scroll", (e) => {
   let Scroll_top = document.querySelector("main").getBoundingClientRect().top;
