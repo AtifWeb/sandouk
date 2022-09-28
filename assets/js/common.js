97bc04ed285e21a5cb9d6790238fc626d76ceea3
Atif Asim <atifasim@Atifs-MacBook-Air.local>
2022-09-28 08:54:09 +0500

    new

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,5 +1,20 @@
 let burger_icon = document.querySelector(".burger_icon");
 
+document.querySelector("body").addEventListener("click", (e) => {
+  console.log(e.target.classList[0]);
+  if (
+    e.target.id != "main_nav_side" &&
+    e.target.classList[0] != "burger_icon"
+  ) {
+    console.log("Inner working");
+    if (document.querySelector("header nav").classList.contains("active")) {
+      document.querySelector("header nav").classList.remove("active");
+    }
+  }
+});
+
 burger_icon.addEventListener("click", (e) => {
-  document.querySelector("header nav").classList.toggle("active");
+  setTimeout(() => {
+    document.querySelector("header nav").classList.toggle("active");
+  }, 100);
 });
